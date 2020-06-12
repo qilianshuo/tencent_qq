6d0de6a1a6e551e3bdc9275b9e2d329b1c1dc2c3
qilianshuo <55940727+qilianshuo@users.noreply.github.com>
2020-06-12 14:15:23 +0800

    Create bkn.cpp

---
+++ bkn计算/bkn.cpp
@@ -0,0 +1,7 @@
+long GetBkn(string skey)
+{
+    var hash = 5381;
+    for (int i = 0, len = skey.Length; i < len; ++i) 
+        hash += (hash << 5) + (int)skey[i];
+    return hash & 2147483647;
+}
